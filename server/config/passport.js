4d596b5d5444d6b9b80494c4c63797774a83fcf8
Lee Ellam <lee.ellam@reedonline.co.uk>
2014-09-03 14:15:52 +0100

    updated auth strategies

--- server/config/passport.js
+++ server/config/passport.js
@@ -28,78 +28,130 @@ passport.use(new LocalStrategy({ usernameField: 'email' }, function(email, passw
   });
 }));
 
+// Local login
+passport.use('local-login', new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password',
+  passReqToCallback: true
+}), function(req, email, password, done) {
+  if (email) email = email.toLowerCase();
+
+  process.nextTick(function() {
+    User.findOne({ 'local.email': email }, function(err, user) {
+      if (err) return done(err);
+
+      if (!user) return done(null, false, req.flash('loginMessage', 'No user found.'));
+
+      if (!user.validPassword(password)) return done(null, false, req.flash('loginMessage', 'Email or password incorrect!'));
+
+      done(null, user);
+    });
+  });
+});
+
+// Local signup
+passport.use('local-signup', new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password',
+  passReqToCallback: true
+}), function(req, email, password, done) {
+  if (email) email = email.toLowerCase();
+
+  process.nextTick(function() {
+    if (!req.user) {
+      User.findOne({ 'local.email' }, function(err, user) {
+        if (err) return done(err);
+
+        if (user) return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
+
+        var newUser = new User();
+
+        newUser.local.email = email;
+        newUser.local.password = password;
+
+        newUser.save(function(err) {
+          if (err) return done(err);
+          return done(null, newUser);
+        });
+      });
+    } else if (!req.user.local.email) {
+      var user = req.user;
+      
+      user.local.email = email;
+      user.local.password = password;
+
+      user.save(function(err) {
+        if (err) return done(err);
+        return done(null, user);
+      });
+    } else {
+      return done(null, req.user);
+    }
+  });
+});
+
+
 /**
  * OAuth Strategy:
  *
- * - User already logged in
- *   - Check if there is an existing account with <provider> id
- *     - If so, return error message
- *     - Else link new OAuth account with current user
  * - User not logged in
- *   - Check if returning user
- *     - If so, sign in
- *     - Else check if there is existing account with email
- *       - If so, return error message
- *       - Else create new account
+ *   - If user with unlinked account exists, link social
+ *   - Else if user exists, log in
+ *   - Else no user, create one
+ * - User already logged in
+ *   - Link account
  */
 
 // Sign in with Facebook
-passport.use(new FacebookStrategy(secrets.facebook, function(req, accessToken, refreshToken, profile, done) {
-  console.log(accessToken);
-  console.log(refreshToken);
-  console.log(profile);
-  if (req.user) {
-    User.findOne({ facebook: profile.id }, function(err, existingUser) {
-      if (existingUser) {
-        console.log('Existing user, already linked!')
-        req.flash('errors', { message: 'There is already a Facebook account that belongs to you. Sign in with that account or delete it and then link it with your current account.' });
-        done(err);
-      } else {
-        User.findById(req.user.id, function(err, user) {
-          console.log('Existing user, linking now!')
-          user.facebook = profile.id;
-          user.tokens.push({ kind: 'facebook', accessToken: accessToken });
-          user.name = user.name || profile.displayName;
-          user.save(function(err) {
-            req.flash('info', { message: 'Facebook account has been linked.' });
-            done(err, user);
-          });
-        });
-      }
-    });
-  } else {
-    User.findOne({ facebook: profile.id }, function(err, existingUser) {
-      if (existingUser) {
-        console.log('Existing facebook user')
-        return done(null, existingUser);
-      }
-      User.findOne({ email: profile._json.email }, function(err, existingEmailUser) {
-        if (existingEmailUser) {
-          console.log('Creating new user, already exists!')
-          //req.flash('errors', { message: 'There is already an account using this email address. Sign in with that account and link it with Facebook from account settings.' });
-          //done(err);
-          console.log(existingEmailUser)
-          existingEmailUser.facebook = profile.id;
-          existingEmailUser.tokens.push({ kind: 'facebook', accessToken: accessToken });
-          existingEmailUser.name = existingEmailUser.name || profile.displayName;
-          existingEmailUser.save(function(err) {
-            req.flash('info', { message: 'Facebook account has been linked.' });
-            done(err, existingEmailUser);
-          });
+passport.use(new FacebookStrategy(secrets.facebook, function(req, token, refreshToken, profile, done) {
+  
+  process.nextTick(function() {
+    if (!req.user) {
+      User.findOne({ 'facebook.id' : profile.id }, function(err, user) {
+        if (err) return done(err);
+
+        if (user) {
+          if (!user.facebook.token) {
+            user.facebook.token = token;
+            user.facebook.id = profile.id;
+            user.facebook.name = profile.displayName;
+            user.facebook.email = (profile.emails[0].value || profile._json.email).toLowerCase();
+
+            user.save(function(err) {
+              if (err) return done(err);
+              return done(null, user);
+            });
+          }
+
+          return done(null, user);
         } else {
-          console.log('Creating new user!')
-          var user = new User();
-          user.email = profile._json.email;
-          user.facebook = profile.id;
-          user.tokens.push({ kind: 'facebook', accessToken: accessToken });
-          user.name = profile.displayName;
-          user.save(function(err) {
-            done(err, user);
+          var newUser = new User();
+          
+          newUser.facebook.token = token;
+          newUser.facebook.id = profile.id;
+          newUser.facebook.name = profile.displayName;
+          newUser.facebook.email = (profile.emails[0].value || profile._json.email).toLowerCase();
+
+          newUser.save(function(err) {
+            if (err) return done(err);
+            return done(null, newUser);
           });
         }
       });
-    });
-  }
+    } else {
+      var user = req.user;
+          
+      user.facebook.token = token;
+      user.facebook.id = profile.id;
+      user.facebook.name = profile.displayName;
+      user.facebook.email = (profile.emails[0].value || profile._json.email).toLowerCase();
+
+      user.save(function(err) {
+        if (err) return done(err);
+        return done(null, user);
+      });
+    }
+  });
 }));
 
 // Middlewares
